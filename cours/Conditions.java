93fdc3f92578c67eeafd9b71c25c661628cdaac4
Alois Patoor <palois89@posteo.net>
2022-10-25 15:37:19 +0200

    Cours conditions done

--- cours/Conditions.java
+++ cours/Conditions.java
@@ -1,28 +1,85 @@
 /**
  * Exercice sur les condtions
  */
-
 package cours_exercices.cours;
 
 // Classe principale
 public class Conditions {
+
     public static void main(String[] args) {
         // Definition d'attributs
-        
-	// Condition if / else
-                
-	// Condition switch
-        
-        // Nouveau switch case depuis JDK 14
-        
-        // Nouveanu switch case sous forme d'expression
-        // Attention au ; a la fin.
+        int a = 5;
+        int b = 10;
 
+        // Condition if / else
+        if (a == b) {
+            System.out.println("a = b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a > b");
+        }
+
+        // Condition switch
+        switch (a) {
+            case 1:
+                System.out.println("a = 1");
+                break;
+            case 2:
+                System.out.println("a = 2");
+                break;
+            case 5:
+                System.out.println("a = 5");
+                break;
+            case 10:
+                System.out.println("a = 10");
+                break;
+            default:
+                System.out.println("a = unknown");
+                break;
+        }
+
+        // Nouveau switch case depuis JDK 14
+        switch (a) {
+            case 0, 1, 2, 3, 4 -> System.out.println("petit chiffre");
+            case 5, 6, 7, 8, 9 -> System.out.println("grand chiffre");
+            default -> System.out.println("Ce n'est plus un chiffre, c'est un nombre");
+        }
+        
+        // Nouveau switch case sous forme d'expression
+        String result = switch (a) {
+            case 0, 1, 2, 3, 4 -> "petit chiffre";
+            case 5, 6, 7, 8, 9 -> "grand chiffre";
+            default -> "Ce n'est plus un chiffre, c'est un nombre";
+        };
+        // Attention au ; a la fin.
+        System.out.println(result);
+        
         // Le nouveau switch case peut etre dans un appel de methode.
+        System.out.println(switch (a) {
+            case 0, 1, 2, 3, 4 -> "petit chiffre";
+            case 5, 6, 7, 8, 9 -> "grand chiffre";
+            default -> "Ce n'est plus un chiffre, c'est un nombre";
+        });
         
         // Nouveau switch sous forme d'expression utilsant yield
+        String result2 = switch (a) {
+            case 0, 1, 2, 3, 4 -> {
+                double sqrt = Math.sqrt(a);
+                yield "petit chiffre dont la racine carrée vaut : " + sqrt;
+            }
+            case 5, 6, 7, 8, 9 -> {
+                double square = a * a;
+                yield "grand chiffre dont le carré vaut : " + square;
+            }
+            default -> "Ce n'est plus un chiffre, c'est un nombre";
+        };
         // (quand le retour se fait sur plusieurs lignes)
+        System.out.println(result2);
         
-	// Condition ternaire
+        // Condition ternaire
+        boolean isTrue = (a == b) ? true : false;
+        System.out.println(isTrue);
+        
     }
 }
--- exercices/conditions/ExConditions.java
+++ exercices/conditions/ExConditions.java
@@ -11,29 +11,74 @@
 package cours_exercices.exercices.conditions;
 
 // Classe principale
+
+import java.util.Scanner;
+
 public class ExConditions {
     public static void main(String[] args) {
         // Declarations / definitions des variables
+        var keyb = new Scanner(System.in);
+        
+        System.out.println("Entrez chiffre 1 : ");
+        int num1 = keyb.nextInt();
+        System.out.println("Entrez chiffre 2 : ");
+        int num2 = keyb.nextInt();
+        System.out.println("Entrez chiffre 3 : ");
+        int num3 = keyb.nextInt();
         
         // Fournir les donnees d'entree
         
         // Chercher le maximum
                 // si num1 > num2 et num1 > num3
+                if (num1 > num2 && num1 > num3){
+                    System.out.println(num1 + "est le plus grand");
+                }
 
                 // si num1 > num2 mais num1 <= num3
+                else if (num1 > num2 && num1 <= num3){
+                    System.out.println(num3 + "est le plus grand");
+                }
 
                 // Si num1 <= num2 et num2 > num3
+                else if (num1 <= num2 && num2 > num3){
+                    System.out.println(num2 + "est le plus grand");
+                }
 
                 // si num1 <= num2 et num2 <= num3
+                else {
+                    System.out.println(num3 + "est le plus grand");
+                }
          
         // Afficher le resultat
-         
+        
         // Afficher si num1 est pair ou impair
+        if (num1 % 2 == 0){
+            System.out.println(num1 + "est pair");
+        } else {
+            System.out.println(num1 + "est impair");
+        }
         
         // Fournir les donnees d'entree
+        System.out.println("Entrez un mois : ");
+        int month = keyb.nextInt();
+        
+        if (month < 1 || month > 12){
+            System.out.println("Erreur, c'est pas un mois, ça, enfin !");
+            month = keyb.nextInt();
+        } else {
+            System.out.println("Vous avez entré le mois " + month);
+        }
+        
+        String result = switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> "Il y a 31 jours dans ce mois";
+            case 4, 6, 9, 11 -> "Il y a 30 jours dans ce mois";
+            default -> "C'est le mois de février";
+        }; 
         
         // Afficher le resultat
+        System.out.println(result);
                 
         // Fermer les ressources
+        keyb.close();
     }
 }
