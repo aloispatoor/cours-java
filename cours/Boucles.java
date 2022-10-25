50647662978f0fa38fbc6033ca21e0e373cf04f3
Alois Patoor <palois89@posteo.net>
2022-10-25 10:47:15 +0200

    initializing project

---
+++ cours/Boucles.java
@@ -0,0 +1,28 @@
+/**
+ * Exemple sur les boucles, continue et break
+ */
+
+package cours_exercices.cours;
+
+// Classe principale
+public class Boucles {
+    public static void main(String[] args) {
+	// Boucle for
+	
+        // Saut de ligne
+	
+	// Boucle do ... while
+        
+        // Saut de ligne
+        
+        // Boucle while
+
+            // utilisation de continue
+
+            // Utilisation de break
+            
+        // Saut de ligne
+        
+        // Boucle "intelligente"
+    }
+}
